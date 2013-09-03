e5c72366d7ec476ef70b780ec1c9354aac24f4a2
Kiran Kulkarni <kk.questworld@gmail.com>
2013-09-04 00:25:21 +0530

    Add basic implementation
    Adds dynamic variables
    1. *hbase-client*
       This hbase-client will be used in subsequent methods
    2. *timeout*
       This lib is using asynchbase which is an asynchronous library.
       However for some calls user will expect synchronous response hence
       library will wait on user's behalf for this timeout.
    
    Adds
    1. get-hbase-client
       simple wrapper over HBaseClient [1] constructor.
       Returns HBaseClient Instance
    2. hbase-wrap
       A macro to which you give your hbase-client and bunch of forms.
       It will bind this hbase-client to dynamic variable *hbase-client*
    3. table-exists?
    4. column-family-exists?
    5. client-stats
       Returns some of the client-stats

--- src/basenji/core.clj
+++ src/basenji/core.clj
@@ -1,6 +1,62 @@
-(ns basenji.core)
+(ns ^{:doc "Basenji Core"
+      :author "Kiran Kulkarni <kk.questworld@gmail.com>"}
+  basenji.core
+  (:import [org.hbase.async HBaseClient ClientStats
+            TableNotFoundException
+            NoSuchColumnFamilyException]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def ^:dynamic *hbase-client* nil)
+(def ^:dynamic *timeout* (* 30 1000))
+
+(defn get-hbase-client
+  ([] (get-hbase-client "localhost" "/hbase"))
+  ([quorum_spec] (get-hbase-client quorum_spec "/hbase"))
+  ([quorum_spec base_path]
+     (HBaseClient. quorum_spec base_path)))
+
+
+(defmacro hbase-wrap
+  [hbase-client & forms]
+  `(binding [*hbase-client* ~hbase-client]
+     ~@forms))
+
+
+(defn table-exists?
+  "Predicate function to check whether given table exists or not"
+  [^String table-name]
+  (try (.. *hbase-client*
+           (ensureTableExists table-name)
+           (join *timeout*))
+       (catch TableNotFoundException _
+         false)))
+
+
+(defn column-family-exists?
+  "Predicat efunction to check whether given table exists and
+   given column-family exists for that table or not"
+  [^String table-name ^String family-name]
+  (let [table-name-bytes (.getBytes table-name)
+        family-name-bytes (.getBytes family-name)]
+    (try (.. *hbase-client*
+             (ensureTableFamilyExists table-name-bytes
+                                      family-name-bytes)
+             (join *timeout*))
+         (catch TableNotFoundException _
+           false)
+         (catch NoSuchColumnFamilyException _
+           false))))
+
+
+(defn get-stats
+  "Returns client stats"
+  []
+  (let [client-stats ^ClientStats (.stats *hbase-client*)]
+    {:connections (.connectionsCreated client-stats)
+     :gets (.gets client-stats)
+     :puts (.puts client-stats)
+     :scans (.scans client-stats)
+     :deletes (.deletes client-stats)
+     :flushes (.flushes client-stats)
+     :rpcs (.numBatchedRpcSent client-stats)
+     :meta-lookups (.uncontendedMetaLookups client-stats)}))

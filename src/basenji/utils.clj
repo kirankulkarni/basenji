4bed80455351b2a5adf2e698dbd193c11ed3ac1e
Kiran Kulkarni <kk.questworld@gmail.com>
2013-09-04 16:45:25 +0530

    Add Byte Array Utils
    Define IByteArray Protocol which has to-byte-array method.
    Extend Clojure and some of Java Types to implement IByteArray
    protocol.
    Add method to get a 2-Dimensional byte-array from sequence/vector of
    byte-arrays

---
+++ src/basenji/utils.clj
@@ -0,0 +1,63 @@
+(ns ^{:doc "Utility functions"
+      :author "Kiran Kulkarni <kk@helpshift.com>"}
+  basenji.utils
+  (:import [org.hbase.async Bytes]))
+
+
+(defprotocol IByteArray
+  (to-byte-array [this]))
+
+
+(extend-protocol IByteArray
+  nil
+  (to-byte-array [x] (byte-array 0))
+
+  java.lang.String
+  (to-byte-array [s] (Bytes/UTF8 s))
+
+  java.lang.Short
+  (to-byte-array [short-int] (Bytes/fromShort short-int))
+
+  java.lang.Integer
+  (to-byte-array [integer] (Bytes/fromInt integer))
+
+  java.lang.Long
+  (to-byte-array [long-int] (Bytes/fromLong long-int))
+
+  java.nio.ByteBuffer
+  (to-byte-array [byte-buffer] (.array byte-buffer))
+
+  clojure.lang.Keyword
+  (to-byte-array [k] (Bytes/UTF8 (name k)) )
+
+  clojure.lang.IPersistentList
+  (to-byte-array [l] (Bytes/UTF8 (binding [*print-dup* false]
+                                   (pr-str l))))
+
+  clojure.lang.APersistentVector
+  (to-byte-array [v] (Bytes/UTF8 (binding [*print-dup* false]
+                                   (pr-str v))))
+  clojure.lang.APersistentMap
+  (to-byte-array [m] (Bytes/UTF8 (binding [*print-dup* false]
+                                   (pr-str m))))
+
+  clojure.lang.APersistentSet
+  (to-byte-array [s] (Bytes/UTF8 (binding [*print-dup* false]
+                                   (pr-str s)))))
+
+
+
+(defn to-byte-array-2d
+  "Shamelessly modified to-array-2d taken from Clojure Core.
+  Returns a (potentially-ragged) 2-dimensional Byte array of Objects
+  containing the contents of coll, which can be any Collection of
+  ByteArrays"
+  {:tag "[[B;"
+   :static true}
+  [^java.util.Collection coll]
+  (let [ret (make-array (. Class (forName "[B")) (. coll (size)))]
+    (loop [i 0 xs (seq coll)]
+      (when xs
+        (aset ret i ^"[B" (first xs))
+        (recur (inc i) (next xs))))
+    ret))

0b1cdfab14f61302f4d0bc8be77a472db398a9e9
Kiran Kulkarni <kk.questworld@gmail.com>
2013-09-04 17:44:41 +0530

    Add `insert` functionality
    Insert accepts Table-Name, RowKey, Column Family, and Qualifier-Values
    map. Inserts it in HBase Automically by creating a PutRequest

--- src/basenji/core.clj
+++ src/basenji/core.clj
@@ -1,9 +1,11 @@
 (ns ^{:doc "Basenji Core"
       :author "Kiran Kulkarni <kk.questworld@gmail.com>"}
   basenji.core
+  (:require [basenji.utils :as bu])
   (:import [org.hbase.async HBaseClient ClientStats
-            TableNotFoundException
-            NoSuchColumnFamilyException]))
+                            TableNotFoundException
+                            NoSuchColumnFamilyException
+                            PutRequest]))
 
 
 (def ^:dynamic *hbase-client* nil)
@@ -62,3 +64,62 @@
      :flushes (.flushes client-stats)
      :rpcs (.numBatchedRpcSent client-stats)
      :meta-lookups (.uncontendedMetaLookups client-stats)}))
+
+
+(defn coerce-qualifier-value-map
+  [qual-val-m]
+  (let [[qualifiers values] (reduce (fn [[qualifiers values] [qual val]]
+                                      (let [qual-byte-array (bu/to-byte-array qual)
+                                            val-byte-array (bu/to-byte-array val)]
+                                        [(conj qualifiers qual-byte-array)
+                                         (conj values val-byte-array)]))
+                                    [[] []]
+                                    qual-val-m)]
+    [(bu/to-byte-array-2d qualifiers)
+     (bu/to-byte-array-2d values)]))
+
+
+(defn- ^PutRequest construct-putrequest
+  [table-name row-key column-family-name qualifiers values timestamp]
+  (if (number? timestamp)
+    (if (instance? (Class/forName "[[B") qualifiers)
+      (PutRequest. ^"[B" table-name
+                   ^"[B" row-key
+                   ^"[B" column-family-name
+                   ^"[[B" qualifiers
+                   ^"[[B" values
+                   (long timestamp))
+      (PutRequest. ^"[B" table-name
+                   ^"[B" row-key
+                   ^"[B" column-family-name
+                   ^"[B" qualifiers
+                   ^"[B" values
+                   (long timestamp)))
+    (if (instance? (Class/forName "[[B") qualifiers)
+      (PutRequest. ^"[B" table-name
+                   ^"[B" row-key
+                   ^"[B" column-family-name
+                   ^"[[B" qualifiers
+                   ^"[[B" values)
+      (PutRequest. ^"[B" table-name
+                   ^"[B" row-key
+                   ^"[B" column-family-name
+                   ^"[B" qualifiers
+                   ^"[B" values))))
+
+
+(defn insert
+  "Atomically Inserts record in HBase."
+  [table-name row-key column-family-name qualifiers-values-map & {timestamp :timestamp}]
+  (let [table (bu/to-byte-array table-name)
+        row (bu/to-byte-array row-key)
+        cf (bu/to-byte-array column-family-name)
+        [quals vals] (coerce-qualifier-value-map qualifiers-values-map)]
+    (.. *hbase-client*
+        (atomicCreate (construct-putrequest table
+                                            row
+                                            cf
+                                            quals
+                                            vals
+                                            timestamp))
+        (join *timeout*))))

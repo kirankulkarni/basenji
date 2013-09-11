9553f90504b7563953eec880b8bfe127aab273ed
Kiran Kulkarni <kk.questworld@gmail.com>
2013-09-11 10:55:46 +0530

    Add info in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject basenji "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject net.kirankulkarni/basenji "0.1.0-SNAPSHOT"
+  :description "A HBase Clojure Client library"
+  :url "https://github.com/kirankulkarni/basenji"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
